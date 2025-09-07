d801e3b04694d28729c7ef1d38425a2a6a61f8f7
Guillaume Postic <gui.postic@gmail.com>
2025-09-08 00:37:18 +0200

    clean

--- starting_point_dynamic_array.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct dynArray DynArray;
-struct dynArray{
-	int* array; // Here, we implement a dynamic array of int
-	size_t used; // The number of used entries
-	size_t size; // The memory allocated to the array is stored here
-};
